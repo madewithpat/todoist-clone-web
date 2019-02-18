9002ede443daf8cb2b75c0605971051c29848e35
kanyetehgiant <47204218+kanyetehgiant@users.noreply.github.com>
2019-02-18 12:46:01 -0500

    ES6 Changes + Edit Comments
    Got rid of bind statements by switching to arrow functions. Wrote a comment about structuring for edit component to replace the delete onClick method.

--- src/TodoItems.js
+++ src/TodoItems.js
@@ -3,15 +3,14 @@ import React, { Component } from 'react';
 class TodoItems extends Component {
 	constructor(props){
 		super(props);
-
-	this.createTasks = this.createTasks.bind(this);	
 	}
+	
 	delete(key) {
 		this.props.delete(key);
 	}
 
 
-	createTasks(item) {
+	createTasks = (item) => {
 		return <li onClick={() => this.delete(item.key)}
 					key={item.key}>{item.text}</li>
 	}
